de2db3c5d453e760120932f6665c56f1d14fe6a9
nigh7m4r3 <chayon.ror@gmail.com>
2017-07-18 01:03:54 +0600

    notification added

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -43,3 +43,21 @@ $$(document).on('pageInit', '.page[data-page="about"]', function (e) {
     // Following code will be executed for page with data-page attribute equal to "about"
     myApp.alert('Here comes About page');
 })
+
+document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal
+    .startInit("65450242-6f58-43b0-bc92-0ff1612e7d0d")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .endInit();
+
+  // Call syncHashedEmail anywhere in your app if you have the user's email.
+  // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
+  // window.plugins.OneSignal.syncHashedEmail(userEmail);
+}, false);
